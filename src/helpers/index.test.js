c189e44c00c227f8d45adf9bde3369c8e876bec3
Abhishek Singh <rajputabhishek003@gmail.com>
2020-07-14 18:17:02 +0530

    getLetterMatchCount method in helper

---
+++ src/helpers/index.test.js
@@ -0,0 +1,18 @@
+import { getLetterMatchCount } from "./index";
+describe("getLetterMatchCount", () => {
+  const secretWord = "party";
+  test("return correct count when there are no matching letters", () => {
+    const letterMatchCount = getLetterMatchCount("bones", secretWord);
+    expect(letterMatchCount).toBe(0);
+  });
+
+  test("return correct count when there are matching letters", () => {
+    const letterMatchCount = getLetterMatchCount("train", secretWord);
+    expect(letterMatchCount).toBe(3);
+  });
+
+  test("return correct count when there are duplicate letters", () => {
+    const letterMatchCount = getLetterMatchCount("parka", secretWord);
+    expect(letterMatchCount).toBe(3);
+  });
+});
