2b271ea77db962c2216ec1e7cce46ab515b138e6
Abhishek Singh <rajputabhishek003@gmail.com>
2020-07-14 21:38:38 +0530

    Input element test case

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,15 +1,35 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const Input = () => {
-  return <div></div>;
+const Input = (props) => {
+  const { success } = props;
+  const content = success ? null : (
+    <form className="form-inline">
+      <input
+        data-test="input-box"
+        className="mb-2 mx-sm-3"
+        type="text"
+        placeholder="Make a guess"
+      />
+      <button
+        type="submit"
+        data-test="submit-button"
+        className="btn btn-primary"
+      >
+        Check{" "}
+      </button>
+    </form>
+  );
+  return <div data-test="component-input">{content}</div>;
 };
-const mapStateToProps = () => {
-    return {
-        
-    }
+const mapStateToProps = ({ success }) => {
+  return {
+    success,
+  };
 };
 
-const mapDispatchToProps = () => {};
+const mapDispatchToProps = () => {
+  return {};
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Input);
--- src/components/input/Input.test.js
+++ src/components/input/Input.test.js
@@ -1,44 +1,49 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Input from "./Input";
-import { findByTestAttribute,storeFactory } from "../../../test/testUtils";
+import { findByTestAttribute, storeFactory } from "../../../test/testUtils";
 
-const setup = (initialState={}) => {
-    const store = storeFactory(initialState)
-  const wrapper = shallow(<Input store={store}/>).dive().dive(); //enzyme method to return the child component in case of HOC etc
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<Input store={store} />)
+    .dive()
+    .dive(); //enzyme method to return the child component in case of HOC etc
   return wrapper;
 };
 
 describe("Check Input Component", () => {
-    describe("Word has not been gussed", () => {
-        test("render component without error",()=>{
-
-        });
-
-        test("render Input box",()=>{
-            
-        });
-
-        test("Render submit button",()=>{
-            
-        });
+  let wrapper;
+  beforeEach(() => {
+    const initialState = { success: false };
+    wrapper = setup(initialState);
+  });
+  
+  describe("Word has not been gussed", () => {
+    test("render component without error", () => {
+        const component = findByTestAttribute(wrapper,'component-input');
+        expect(component.length).toBe(1);
     });
 
-    describe("Word has been gussed", () => {
-        test("render component without error",()=>{
-
-        });
-
-        test("Does not render Input box",()=>{
-            
-        });
-
-        test("does not render submit button",()=>{
-            
-        });
+    test("render Input box", () => {
+        const component = findByTestAttribute(wrapper,'input-box');
+        expect(component.length).toBe(1);
     });
+
+    test("Render submit button", () => {
+        const component = findByTestAttribute(wrapper,'submit-button');
+        expect(component.length).toBe(1);
+    });
+  });
+
+  describe("Word has been gussed", () => {
+    test("render component without error", () => {});
+
+    test("Does not render Input box", () => {});
+
+    test("does not render submit button", () => {});
+  });
 });
 
 describe("Update state on dispatch", () => {
-    test("Initial Render of Element", () => {});
-  });
+  test("Initial Render of Element", () => {});
+});
